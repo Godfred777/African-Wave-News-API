6a0145d54bc185b7c1dd19e3c724daf6074aabf9
Godfred777 <81054545+Godfred777@users.noreply.github.com>
2025-03-12 21:09:20 +0000

    Added a function to detect language from content

--- src/config/i18n.mjs
+++ src/config/i18n.mjs
@@ -1,6 +1,7 @@
 import i18next from "i18next";
 import Backend from "i18next-fs-backend";
 import middleware from "i18next-http-middleware";
+import { franc } from "franc-min";
 
 i18next
   .use(Backend)
@@ -19,6 +20,23 @@ i18next
     interpolation: {
       escapeValue: false,
     },
-  });
+  }
+);
+
+
+export function detectLanguageFromContent(content) {
+    const language = franc(content, { minLength: 1 });
+    const supportedLngs = i18next.options.supportedLngs;
+
+    if (supportedLngs.includes(language)) {
+        if (language === "fra") return "fr";
+        if (language === "deu") return "de";
+        if (language === "spa") return "es";
+        return "en";
+    } else {
+        return console.error(`Language not supported: ${language}`);
+    }
+    
+}
 
 export default i18next;
